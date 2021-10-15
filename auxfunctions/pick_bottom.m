df31d08b7a17956140ffec693dcb9d745a47a089
lsmuhle <leahsophie.muhle@gmail.com>
2021-10-15 13:07:01 +0200

    Update pick_bottom.m
    - adapted to more input types

--- auxfunctions/pick_bottom.m
+++ auxfunctions/pick_bottom.m
@@ -1,24 +1,32 @@
 function [geoinfo] = pick_bottom(geoinfo, tp, opt, MinBottomPick, MaxBottomPick)
-    
-    if strcmpi(opt.input_type, 'MCoRDS')
-        data_scaled = mag2db(geoinfo.data);
-    elseif strcmpi(opt.input_type, 'GPR_LF')
-        data_scaled = geoinfo.data;
-    end
-    
-    horizontal_mean = mean(data_scaled,2);
-    normalized_data = data_scaled - horizontal_mean;
 
-    BottomInds = zeros(1,geoinfo.num_trace);
-    [~,BottomInds(1)] = max(normalized_data(MinBottomPick(1):MaxBottomPick(1),1));
-    for n=2:geoinfo.num_trace
-        [~,Ind] = findpeaks(normalized_data(MinBottomPick(n):MaxBottomPick(n),n),'SortStr','descend','NPeaks',tp.num_bottom_peaks);
-        [~, pos] = min(abs(Ind-BottomInds(n-1)));
-        BottomInds(n) = Ind(pos);
+    if ~(strcmpi(opt.input_type, 'GPR_HF') || strcmpi(opt.input_type, 'awi_flight'))
+
+    
+        if strcmpi(opt.input_type, 'MCoRDS')
+            data_scaled = mag2db(geoinfo.data);
+        elseif strcmpi(opt.input_type, 'GPR_LF')
+            data_scaled = geoinfo.data;
+        end
+
+        horizontal_mean = mean(data_scaled,2);
+        normalized_data = data_scaled - horizontal_mean;
+
+        BottomInds = zeros(1,geoinfo.num_trace);
+        [~,BottomInds(1)] = max(normalized_data(MinBottomPick(1):MaxBottomPick(1),1));
+        for n=2:geoinfo.num_trace
+            [~,Ind] = findpeaks(normalized_data(MinBottomPick(n):MaxBottomPick(n),n),'SortStr','descend','NPeaks',tp.num_bottom_peaks);
+            [~, pos] = min(abs(Ind-BottomInds(n-1)));
+            BottomInds(n) = Ind(pos);
+        end
+
+        BottomInds = floor(movmean(BottomInds,tp.smooth_bot));
+        BottomInds = BottomInds + MinBottomPick;
+
+        geoinfo.twt_bot = ind2twt(geoinfo,BottomInds);
+        
+    else
+        geoinfo.twt_bot = NaN(1,geoinfo.num_trace);
     end
 
-    BottomInds = floor(movmean(BottomInds,tp.smooth_bot));
-    BottomInds = BottomInds + MinBottomPick;
-    
-    geoinfo.twt_bot = ind2twt(geoinfo,BottomInds);
 end
