f56239b2f686161e195146c2e14675cfee0157b4
lsmuhle <leahsophie.muhle@gmail.com>
2021-10-15 12:56:15 +0200

    Create find_max_min.m
    introduce function to find intensity minima and maxima to avoid repetitive code

---
+++ auxfunctions/find_max_min.m
@@ -0,0 +1,25 @@
+function [lind, p] = find_max_min(opt, data)
+%FIND_MAX_MIN Summary of this function goes here
+%   Detailed explanation goes here
+
+
+if opt.find_maxima
+    % find local maxima
+    [lind,p] = islocalmax(data);
+    p = p(lind); 
+    lind = find(lind);
+    if isempty(lind)
+        [~,lind,~,p] = findpeaks(data); % multiply data with -1 to find its minima
+    end
+else
+    % find local minima
+    [lind,p] = islocalmin(data);
+    p = p(lind); 
+    lind = find(lind);
+    if isempty(lind)
+        [~,lind,~,p] = findpeaks(-1*data);  % multiply data with -1 to find its minima
+    end
+end
+
+end
+
