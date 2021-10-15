cd767a0994e366d2a496cb02ffbe6f4b7a88dba6
lsmuhle <leahsophie.muhle@gmail.com>
2021-10-15 13:04:45 +0200

    Update file_interpreter.m

--- auxfunctions/file_interpreter.m
+++ auxfunctions/file_interpreter.m
@@ -13,10 +13,10 @@ function [opt] = file_interpreter(opt)
     if opt.load_crossover
         filenames_cross = {};
         if strcmp(opt.cross_section,'all')
-            cross_struct = dir(append(pwd, opt.output_folder,'\*.mat'));
+            cross_struct = dir(append(pwd, opt.output_folder,'/*.mat'));
             opt.n_cross = length(cross_struct);
             for k = 1:opt.n_cross
-                filename_cross = append(cross_struct(k).folder, '\', cross_struct(k).name);
+                filename_cross = append(cross_struct(k).folder, '/', cross_struct(k).name);
                 filenames_cross = [filenames_cross; filename_cross];
             end
         else
