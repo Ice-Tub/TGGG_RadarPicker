4d008c3e7c131a5077d50ca1a52a0083c2cd2f50
lsmuhle <36638264+lsmuhle@users.noreply.github.com>
2022-02-22 13:52:15 +0100

    compute how many interruptions occur per picked layer

---
+++ auxfunctions/compute_interruption.m
@@ -0,0 +1,59 @@
+function [interruption] = compute_interruption(layer)
+%COMPUTE_INTERRUPTION Creates array which contains 1 at positions where
+%picked layer is interrupted, otherwise it contains 0
+logArr = ~isnan(layer);
+interruption = zeros(size(layer));
+
+% if NaN occurs at a position where picked layer can be found at both sides
+% of it, it is considered an interruption. However, an interruption is only
+% valid if it continues for more than 10 pixel
+for ii = 2:length(layer)-1
+    if isnan(layer(ii))
+        if sum(logArr(1:ii-1)) > 0 && sum(logArr(ii+1:end)) > 0
+            interruption(ii) = 1;
+        end
+    end
+
+end
+
+% calculate how many interruptions occur (only if they are longer than 10
+% pixel)
+
+limitInterruption = 10;
+count = 0;
+numberOfInterruptions = 0;
+
+if sum(interruption) > limitInterruption
+
+    indInterruption = find(interruption);
+    currentInd = indInterruption(1);
+
+    for kk = 1:length(indInterruption)
+        if currentInd == indInterruption(kk)
+            count = count + 1;
+            currentInd = indInterruption(kk) + 1;
+        else
+            if count > limitInterruption
+                numberOfInterruptions  = numberOfInterruptions + 1;
+                count = 1;
+                currentInd = indInterruption(kk) + 1;
+            else
+                count = 1;
+                currentInd = indInterruption(kk) + 1;
+            end
+    
+        end
+    
+    end
+
+    if count > limitInterruption
+        numberOfInterruptions = numberOfInterruptions + 1;
+    end
+
+end
+
+interruption = strcat(string(numberOfInterruptions), ' interruption(s)');
+
+end
+
+
