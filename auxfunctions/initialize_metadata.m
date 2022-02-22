f0a6048bc11ee833619a23d2fe39bab6918b88c6
lsmuhle <36638264+lsmuhle@users.noreply.github.com>
2022-02-22 13:53:06 +0100

    create initialize_metadata
    to create metadata file

---
+++ auxfunctions/initialize_metadata.m
@@ -0,0 +1,16 @@
+function [metadata] = initialize_metadata(geoinfo, opt)
+%CREATE_METADATA Summary of this function goes here
+%   Detailed explanation goes here
+metadata.coordinator = opt.coordinator;
+metadata.picker = opt.picker;
+metadata.lat = geoinfo.lat;
+metadata.lon = geoinfo.lon;
+metadata.psX = geoinfo.psX;
+metadata.psY = geoinfo.psY;
+metadata.twt = geoinfo.twt;
+metadata.radarType = opt.input_type;
+metadata.frequency = opt.frequency;
+metadata.crossover = {};
+
+end
+
