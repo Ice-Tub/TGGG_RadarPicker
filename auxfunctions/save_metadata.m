7552fa394846ef7997ffc2ea5acfad4906bdd13c
lsmuhle <36638264+lsmuhle@users.noreply.github.com>
2022-02-24 16:56:36 +0100

    save_metadata updated

--- auxfunctions/save_metadata.m
+++ auxfunctions/save_metadata.m
@@ -1,6 +1,18 @@
-function save_metadata(opt, metadata)
+function save_metadata(opt, geoinfo, metadata)
 %SAVE_METADATA Save metadata
 %   
+
+%compute interruptions in layer for metadata
+for ii = 1:opt.nol
+    metadata.interruptions{ii} = compute_interruption(geoinfo.layers(ii,:));
+end
+
+% save picked layers with bins and twt in metadata
+dt = geoinfo.twt(2) - geoinfo.twt(1);
+metadata.IRH_bin = geoinfo.layers;
+metadata.IRH_twt = geoinfo.layers .* dt;
+metadata.qualities = geoinfo.qualities;
+
 
 % check if old metadata exists and, if so, replace the dates of newly
 % picked layers
@@ -14,6 +26,8 @@ if isfile(opt.file_metadata)
         end
     end  
 end
+
 save(opt.file_metadata, '-struct', 'metadata');
+
 end
 
