5a5c4eb7c07fee3fa88cd9becc1e6f6f1c363caf
lsmuhle <36638264+lsmuhle@users.noreply.github.com>
2022-02-24 16:56:06 +0100

    initialize_metadata updated

--- auxfunctions/initialize_metadata.m
+++ auxfunctions/initialize_metadata.m
@@ -2,17 +2,22 @@ function [metadata] = initialize_metadata(geoinfo, opt)
 %CREATE_METADATA Summary of this function goes here
 %   Detailed explanation goes here
 metadata.coordinator = opt.coordinator;
-metadata.picker = opt.picker;
+metadata.operator = opt.picker;
 metadata.lat = geoinfo.lat;
 metadata.lon = geoinfo.lon;
 metadata.psX = geoinfo.psX;
 metadata.psY = geoinfo.psY;
 metadata.twt = geoinfo.twt;
-metadata.radarType = opt.input_type;
 metadata.frequency = opt.frequency;
 metadata.crossover = {};
 metadata.pickingDates(1:opt.nol) = {'not picked'};
 metadata.interruptions(1:opt.nol) = {'not picked'};
+
+if strcmp(opt.input_type, 'GPR_LF')
+    metadata.radarType = 'Ground-based dipole antennae';
+elseif strcmp(opt.input_type, 'MCoRDS')
+    metadata.radarType = 'airborne MCoRDS';
+end
 
 end
 
