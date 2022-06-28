232c146c1a6f2012bed6168df1a003871594571c
inkakoch <inka.koch@uni-tuebingen.de>
2022-06-28 10:46:25 +0200

    new example trys

--- edit_datafiles.m
+++ edit_datafiles.m
@@ -3,12 +3,12 @@ close all;
 addpath(append(pwd,'\auxfunctions'))
 
 % Settings
-move_option = 'update' ; % Options: 'swap', 'overwrite', 'view' or 'update'.
-data_folder = 'data\picked_mcords';
-move_from_file = 'LayerData_002';
-move_from_layer = 1;
-move_to_file = 'LayerData_002';
+move_option = 'overwrite' ; % Options: 'swap', 'overwrite', 'view' or 'update'.
+data_folder = 'data\MCoRDS_picked_corrected';
+move_from_file = 'LayerData_06_02_002';
+move_from_layer = 9;
+move_to_file = 'LayerData_06_02_002';
 
-move_to_layer = 1;
+move_to_layer = 10;
 
 edit_data(move_option, data_folder, move_from_file, move_from_layer, move_to_file, move_to_layer);
