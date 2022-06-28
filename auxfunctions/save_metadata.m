6e5700e35618358f312f652a4fc12eab99639ede
lsmuhle <36638264+lsmuhle@users.noreply.github.com>
2022-06-28 11:12:04 +0200

    Update save_metadata.m
    Delete that pickingDates are saved

--- auxfunctions/save_metadata.m
+++ auxfunctions/save_metadata.m
@@ -13,20 +13,6 @@ metadata.IRH_bin = geoinfo.layers;
 metadata.IRH_twt = geoinfo.layers .* dt;
 metadata.qualities = geoinfo.qualities;
 
-
-% check if old metadata exists and, if so, replace the dates of newly
-% picked layers
-if isfile(opt.file_metadata)
-    oldMetadata = load(opt.file_metadata);
-    for ii = 1:opt.nol
-        if ~strcmp(oldMetadata.pickingDates{ii}, 'not picked')
-            if strcmp(metadata.pickingDates{ii}, 'not picked')
-                metadata.pickingDates{ii} = oldMetadata.pickingDates{ii}; 
-            end
-        end
-    end  
-end
-
 save(opt.file_metadata, '-struct', 'metadata');
 
 end
