ec38b3f280fa2d010ba3064e90431a51b581d375
lsmuhle <36638264+lsmuhle@users.noreply.github.com>
2022-07-14 15:21:26 +0200

    updates to improve metadata

--- auxfunctions/save_metadata.m
+++ auxfunctions/save_metadata.m
@@ -13,9 +13,15 @@ metadata.IRH_bin = geoinfo.layers;
 metadata.IRH_twt = geoinfo.layers .* dt;
 metadata.qualities = geoinfo.qualities;
 
-% save row containing file_name for each trace
+% save row containing filename etc. for each trace
 metadata.filename = repmat({opt.input_file}, 1, size(metadata.IRH_bin,2));
+metadata.acquisition_day = repmat(opt.acquisition_day, 1, size(metadata.IRH_bin,2));
+metadata.acquisition_month = repmat(opt.acquisition_month, 1, size(metadata.IRH_bin,2));
+metadata.acquisition_year = repmat(opt.acquisition_year, 1, size(metadata.IRH_bin,2));
+metadata.survey_number = repmat(opt.survey_number, 1, size(metadata.IRH_bin,2));
+metadata.profile_number = repmat(opt.profile_number, 1, size(metadata.IRH_bin,2));
 metadata.trace = 1:size(metadata.IRH_bin,2);
+metadata.picking_date = repmat(opt.picking_date, 1, size(metadata.IRH_bin,2));
 
 save(opt.file_metadata, '-struct', 'metadata');
 
--- mergeLayers.m
+++ mergeLayers.m
@@ -45,9 +45,15 @@ merged.IRH_twt = allMetadata.metadata1.IRH_twt;
 %merged.IRH_RelSurfBin = allMetadata.metadata1.IRH_relto_surf_bin;
 merged.IRH_RelSurfTwt = allMetadata.metadata1.IRH_relto_surf_twt;
 merged.qualities = allMetadata.metadata1.qualities;
-merged.interruption = allMetadata.metadata1.interruptions;
+%merged.interruption = allMetadata.metadata1.interruptions;
+merged.acquisition_year = allMetadata.metadata1.acquisition_year;
+merged.acquisition_month = allMetadata.metadata1.acquisition_month;
+merged.acquisition_day = allMetadata.metadata1.acquisition_day;
+merged.survey_number = allMetadata.metadata1.survey_number;
+merged.profile_number = allMetadata.metadata1.profile_number;
 merged.filename = allMetadata.metadata1.filename;
 merged.trace = allMetadata.metadata1.trace; 
+merged.picking_date = allMetadata.metadata1.picking_date; 
 
 for kk = 2:numberFiles
     currentName = append('metadata', num2str(kk));
@@ -68,9 +74,15 @@ for kk = 2:numberFiles
     %merged.IRH_RelSurfBin = cat(2,merged.IRH_RelSurfBin, allMetadata.(currentName).IRH_relto_surf_bin);
     merged.IRH_RelSurfTwt = cat(2,merged.IRH_RelSurfTwt, allMetadata.(currentName).IRH_relto_surf_twt);
     merged.qualities = cat(2,merged.qualities, allMetadata.(currentName).qualities);
-    merged.interruption =  cat(1, merged.interruption, allMetadata.(currentName).interruptions);
+    %merged.interruption =  cat(1, merged.interruption, allMetadata.(currentName).interruptions);
+    merged.acquisition_year = cat(2,merged.acquisition_year, allMetadata.(currentName).acquisition_year);
+    merged.acquisition_month = cat(2,merged.acquisition_month, allMetadata.(currentName).acquisition_month);
+    merged.acquisition_day = cat(2,merged.acquisition_day, allMetadata.(currentName).acquisition_day);
+    merged.survey_number = cat(2,merged.survey_number, allMetadata.(currentName).survey_number);
+    merged.profile_number = cat(2,merged.profile_number, allMetadata.(currentName).profile_number);
     merged.filename = cat(2,merged.filename, allMetadata.(currentName).filename);
     merged.trace = cat(2,merged.trace, allMetadata.(currentName).trace);
+    merged.picking_date = cat(2, merged.picking_date, allMetadata.(currentName).picking_date);
 end
 
 % split the IRH into the individual layers
@@ -87,11 +99,11 @@ end
 
 % save bottom only if desired
 if save_bottom
-    namesVariables = {'lon', 'lat', 'psX', 'psY',  'trace', 'filename', 'surface', 'base'};
-    mergedTable = table(merged.lon', merged.lat', merged.psX', merged.psY', merged.trace', merged.filename',merged.surfaceTwt', merged.bottomRelSurfTwt',  'VariableNames', namesVariables);
+    namesVariables = {'year_acq', 'month_acq', 'day_acq', 'surveyID', 'profileID', 'lon', 'lat', 'psX', 'psY',  'trace', 'filename', 'surface', 'base', 'year_pick', 'month_pick', 'day_pick'};
+    mergedTable = table(merged.acquisition_year', merged.acquisition_month', merged.acquisition_day',merged.survey_number', merged.profile_number', merged.lon', merged.lat', merged.psX', merged.psY', merged.trace', erase(merged.filename,'.mat')', merged.surfaceTwt', merged.bottomRelSurfTwt', year(merged.picking_date)', month(merged.picking_date)', day(merged.picking_date)', 'VariableNames', namesVariables);
 else
-    namesVariables = {'lon', 'lat', 'psX', 'psY', 'trace', 'filename', 'surface'};
-    mergedTable = table(merged.lon', merged.lat', merged.psX', merged.psY',  merged.trace', merged.filename',merged.surfaceTwt', 'VariableNames', namesVariables);
+    namesVariables = {'year_acq', 'month_acq', 'day_acq', 'surveyID', 'profileID', 'lon', 'lat', 'psX', 'psY', 'trace', 'filename', 'surface', 'year_pick', 'month_pick', 'day_pick'};
+    mergedTable = table(merged.acquisition_year', merged.acquisition_month', merged.acquisition_day',merged.survey_number', merged.profile_number', merged.lon', merged.lat', merged.psX', merged.psY',  merged.trace', erase(merged.filename, '.mat')', merged.surfaceTwt', year(merged.picking_date)', month(merged.picking_date)', day(merged.picking_date)', 'VariableNames', namesVariables);
 end
 
 % save twt of layers first 
@@ -129,7 +141,7 @@ end
 writetable(mergedTable, append(pwd,'/data/metadata/txtfiles', outputFileNameIRH), 'delimiter', ',')
 
 % write remaining info to extra file
-infoCell = {append('frequency: ', merged.frequency), append('radar type: ', merged.radarType), append('Picking date: ', date)};
+infoCell = {append('frequency: ', merged.frequency), append('radar type: ', merged.radarType)};
 
 infoCell{end+1} = [];
 
@@ -139,12 +151,17 @@ for mm = 1:numberFiles
     currentData = append('metadata', num2str(mm));
     currentFilename = char(allMetadata.(currentData).filename(1));
     infoCell{end+1} = append('original filename', num2str(mm), ': ', currentFilename);
-    infoCell{end+1} = append('acquisition date', num2str(mm), ': ');
+    %infoCell{end+1} = append('acquisition date', num2str(mm), ': ');
     %infoCell{end+1} = [append("interruption", num2str(mm), ": "), merged.interruption(mm,:)];
     infoCell{end+1} = [];
 end
 
 infoCell{end+1} = 'Column headers are:';
+infoCell{end+1} = 'year_acq: acquisition year';
+infoCell{end+1} = 'month_acq: acquisition month';
+infoCell{end+1} = 'day_acq: acquisition day';
+infoCell{end+1} = 'surveyID: survey profile number';
+infoCell{end+1} = 'profileID: survey line number';
 infoCell{end+1} = 'lon: x-coordinate in decimal degrees';
 infoCell{end+1} = 'lat: y-coordinate in decimal degrees';
 infoCell{end+1} = 'psX: x-coordinate in polar stereographic project 71 south (EPSG:3031)';
@@ -153,6 +170,9 @@ infoCell{end+1} = 'trace: trace number of the radar transect along-track';
 infoCell{end+1} = 'surface: two-way travel time of the surface (when applicable)';
 infoCell{end+1} = 'base: two-way travel time of the ice base in seconds from the surface';
 infoCell{end+1} = 'IRHx: two-way travel time of the IRH x in seconds from the surface';
+infoCell{end+1} = 'year_pick: year the IRHs were last picked or edited';
+infoCell{end+1} = 'month_pick: month the IRHs were last picked or edited';
+infoCell{end+1} = 'day_pick: day the IRHs were last picked or edited';
 
 
 infoCell = infoCell';
