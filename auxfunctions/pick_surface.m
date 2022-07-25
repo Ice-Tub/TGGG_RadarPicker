b8d10aaaac6cc94448850442875daefd184f5683
lsmuhle <36638264+lsmuhle@users.noreply.github.com>
2022-07-25 14:51:41 +0200

    Update pick_surface.m
    Surface pick twt of PulsEKKO is assumed to be 0.

--- auxfunctions/pick_surface.m
+++ auxfunctions/pick_surface.m
@@ -1,16 +1,30 @@
 function [geoinfo] = pick_surface(geoinfo,tp,opt)
 
-    if strcmpi(opt.input_type, 'MCoRDS') || strcmpi(opt.input_type, 'PulsEKKO')
+% the first maximum should be picked for the surface
+    if strcmpi(opt.input_type, 'MCoRDS') 
+       
+        % choose the first of the first ten maxima
         [~,FirstArrivalInds] = max(geoinfo.data(tp.MinBinForSurfacePick:end,:));
         FirstArrivalInds = floor(movmean(FirstArrivalInds,tp.smooth_sur));
         FirstArrivalInds = FirstArrivalInds+tp.MinBinForSurfacePick;
 
         geoinfo.twt_sur=ind2twt(geoinfo,FirstArrivalInds);
         
-    elseif strcmpi(opt.input_type, 'GPR_LF') || strcmpi(opt.input_type, 'GPR_HF') || strcmpi(opt.input_type, 'awi_flight') 
+    elseif strcmpi(opt.input_type, 'GPR_LF') || strcmpi(opt.input_type, 'GPR_HF') || strcmpi(opt.input_type, 'awi_flight') || strcmpi(opt.input_type, 'PulsEKKO')
         geoinfo.twt_sur = zeros(1,geoinfo.num_trace);
-        disp("Traveltime of surface pick for GPR_LF data is set to 0.")
-    end
+        disp("Traveltime of surface pick is set to 0.")
+
+%     elseif strcmpi(opt.input_type, 'PulsEKKO')
+%      % for each trace, choose the first of 5 maxima as the surface pick
+%         for ii = 1:geoinfo.num_trace
+%             [~,indPeaks] = findpeaks(geoinfo.data(tp.MinBinForSurfacePick:end, ii), 'NPeaks', 5);
+%             correctedInd = indPeaks(1) + tp.MinBinForSurfacePick;
+%             geoinfo.twt_sur(ii) = ind2twt(geoinfo, correctedInd);
+%         end
+%     end
+
+
+
 end
 
  
