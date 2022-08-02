68bdf08201153bfbe8e02f51e95a6d1377f2e10e
oraschewski <36086882+oraschewski@users.noreply.github.com>
2022-08-02 16:00:00 +0200

    Allow absolute and relative folder paths
    Allow input and output folder paths to be given as absolute or relative paths.
    
    Fix bug, where crosspoints are searched in the currently processed geoinfo.

--- auxfunctions/file_interpreter.m
+++ auxfunctions/file_interpreter.m
@@ -7,24 +7,30 @@ function [opt] = file_interpreter(opt)
     if ~contains(opt.output_suffix, '.mat')
         opt.output_suffix = append(opt.output_suffix, '.mat');
     end
-
-    opt.filename_input_data = append(pwd, opt.input_folder, opt.input_file); % Inputfile not needed if geoinfofile already exists.
-    opt.filename_geoinfo = append(pwd, opt.output_folder, opt.output_prefix, opt.output_suffix);
+    
+    % Get full input filename (Not needed if geoinfo file already exists)
+    input_path = dir(fullfile(opt.input_folder, opt.input_file)); % Obtain abolute path and file name, independent of 'opt.input_folder' being an absolute or relative path.
+    opt.filename_input_data = fullfile(input_path.folder, input_path.name); % Get the full input file name.
+    
+    % Get full output filename
+    output_file = append(opt.output_prefix, opt.output_suffix); % Merge output prefix and suffix
+    output_path = dir(fullfile(opt.output_folder, output_file)); % Obtain abolute path and file name, independent of 'opt.output_folder' being an absolute or relative path.
+    opt.filename_geoinfo = fullfile(output_path.folder, output_path.name); % Get the full output file name.
+    
     if opt.load_crossover
         filenames_cross = {};
         if strcmp(opt.cross_section,'all')
-            cross_struct = dir(append(pwd, opt.output_folder,'/*.mat'));
-            opt.n_cross = length(cross_struct);
-            for k = 1:opt.n_cross
-                filename_cross = append(cross_struct(k).folder, '/', cross_struct(k).name);
+            cross_struct = dir(fullfile(pwd, opt.output_folder,'*.mat'));
+            for k = 1:length(cross_struct)
+                filename_cross = fullfile(cross_struct(k).folder, cross_struct(k).name);
                 filenames_cross = [filenames_cross; filename_cross];
             end
         else
+            % ToDO: recheck that this option works
             if ischar(opt.cross_section)
                 opt.cross_section = {opt.cross_section};
             end
-            opt.n_cross = numel(opt.cross_section);
-            for k = 1:opt.n_cross
+            for k = 1:numel(opt.cross_section)
                 filename_cross = append(pwd,opt.output_folder,opt.output_prefix,opt.cross_section{k},'.mat');
                 filenames_cross = [filenames_cross; filename_cross];
             end
