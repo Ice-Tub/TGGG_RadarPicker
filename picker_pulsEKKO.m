c70f6bf7cfde28febae0a87dbbd8cb72375f2a74
oraschewski <36086882+oraschewski@users.noreply.github.com>
2022-10-06 15:47:53 +0200

    Update picker_pulsEKKO.m

--- picker_pulsEKKO.m
+++ picker_pulsEKKO.m
@@ -1,4 +1,4 @@
-clear all;
+clear;
 close all;
 
 % - - - - - - - - -- - - - - - - - - - - - - - - - - - - - - - - -
@@ -12,17 +12,9 @@ close all;
 
 % Working settings
 % Data
-opt.input_type = 'PulsEKKO'; %Inputfile-type, options: 'GPR_LF', 'MCoRDS', 'GPR_HF', 'awi_flight', 'PulsEKKO'
-opt.input_folder = '/data/PulsEKKO/';
-opt.input_file = 'AlongFlow2';
-opt.output_folder = '/data/picked_data/PulsEKKO/';
-opt.output_prefix = '/LayerData_'; % Define a prefix for the layerdata-file. (output_file = prefix + suffix)
-opt.output_suffix = 'AlongFlow2'; % Define a suffix for the layerdata-file.
-opt.file_metadata = strcat(pwd,'/data/metadata/',opt.output_suffix,'_metadata.mat');
-opt.cross_section = 'all'; % Options : List of numbers (e.g.:{'001'; '002'}) or all files in output_folder('all'). Some already pick section to find cross-points.
 opt.coordinator = 'Reinhard Drews';
 opt.picker = 'Leah Sophie Muhle, Inka Koch';
-opt.frequency = '5 MHz';
+opt.frequency = '50 MHz';
 opt.picking_date = datetime('today');
 opt.acquisition_day = NaN;
 opt.acquisition_month = NaN;
@@ -31,35 +23,50 @@ opt.survey_number = NaN;
 opt.profile_number = NaN; % needs to be a number
 
 
+opt.input_type = 'PulsEKKO'; %Inputfile-type, options: 'GPR_LF', 'MCoRDS', 'GPR_HF', 'awi_flight', 'PulsEKKO'
+opt.input_folder = '/../../data/rd_group/ReMeltRadar/Proc_PE/';
+opt.input_file = 'EIS_along1';
+opt.output_folder = '/../../data/rd_group/ReMeltRadar/Proc_PE/picks/'; % Relative or absolue path
+opt.output_prefix = '/LayerData_'; % Define a prefix for the layerdata-file. (output_file = prefix + suffix)
+opt.output_suffix = opt.input_file; % Define a suffix for the layerdata-file.
+opt.metadata_folder = '/../../data/rd_group/ReMeltRadar/Proc_PE/picks/metadata/';
+opt.metadata_prefix = '/metadata_'; % Define a prefix for the metadata-file. (metadata_file = prefix + suffix)
+opt.cross_section = 'EIS_along2'; % Options : List of numbers (e.g.:{'001'; '002'}) or all files in output_folder('all'). Some already pick section to find cross-points.
+
+
+
+
 % Options
 % The following options can be activated by setting: 1 = yes, 0 = no.
 opt.create_new_geoinfo = 0; % CAUTION: Already picked layer for this echogram will be overwritten, if keep_old_picks = 0.
-opt.update_bottom = 0;      % Update bottom, when old geoinfo is loaded.
-opt.update_seeds = 1;       % This option can be used to update seeds only, if bottom is updated or a new geoinfo is created, the seeds will be computed in any case.
+opt.update_bottom = 1;      % Update bottom, when old geoinfo is loaded.
+opt.update_seeds = 1;      % Update bottom, when old geoinfo is loaded.
+opt.manual_bottom_pick = 1; % Pick bottom manually, needed for data with intereupted or weak bottom reflector.
 opt.keep_old_picks = 1;     % Keep old picks, when old geoinfo is loaded.
 opt.load_crossover = 1;     % Activate loading cross-over points.
-opt.delete_stripes = 0;     % If activated, the horizontal mean will be subtracted from data.01 
-opt.filter_frequencies = 1; % If activated, frequencies higher than 10 MHz will be filtered from signal; applies only to GPR_LF data
+
+opt.delete_stripes = 0;     % If activated, the horizontal mean will be subtracted from data.
+opt.filter_frequencies = 0; % If activated, frequencies higher than 10 MHz will be filtered from signal; applies only to GPR_LF data
 opt.median_peaks = 1;       % If activated, not the intensity peaks, but the median intensities from a tp.window x tp.window environment will be used for findpeaks
 opt.interpol_peaks = 0;     % If activated, linear interpolation of all the peaks in a tp.window x tp.window field are computed for layer propagation. This and medianpeaks should not be activated simultaneously. 
 opt.nopeak_step = 1;        % Use the previous step instead of the same height for propagate_layer if no local extremum of intensity can be found
 opt.find_maxima = 0;        % If activated, maxima are picked. Otherwise minima are picked. 
 
 % Appearance
-opt.len_color_range = 0.05;
-opt.cmp = 'bone';            % e.g. 'jet', 'bone'
+opt.len_color_range = 0.8;
+opt.cmp = 'bone';            % e.g. 'jet', 'bone', 'gray'
 opt.editing_window = 10;    % Number of traces that are updated in editing mode.
 
 %%% Tuning parameters
 % For cutting the data
 tp.clms='all_clms';              % If an existing file is loaded, this option is overwritten.
-tp.rows=1:4000;                % cuts the radargram to limit processing (time) (top and bottom)
+tp.rows='all_rows';%1:2500;                % cuts the radargram to limit processing (time) (top and bottom)
 
 % For surface and bottom pick
 tp.MinBinForSurfacePick = 10; % when already preselected, this can be small
 tp.smooth_sur=40;             % between 30 and 60 seems to be good
-tp.MinBinForBottomPick = 200;
-tp.MaxBinForBottomPick = 700;
+tp.MinBinForBottomPick = 8000;
+tp.MaxBinForBottomPick = 11000;
 tp.num_bottom_peaks = 3;      % Number of strongest peaks considered as bottom pick. 10 is a good guess.
 tp.smooth_bot = 1;              % Smoothing for bottom pick. No smoothing for smooth_bot 
 
